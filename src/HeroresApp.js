c00fa26da75a8d3407c41d312b56fb8013b921f6
Marcos García <garciafmarcos@gmail.com>
2021-11-07 20:12:39 +0100

    Renovar JSON.parse

--- src/HeroresApp.js
+++ src/HeroresApp.js
@@ -4,7 +4,8 @@ import { authReducer } from './auth/authReducer';
 import { AppRouter } from './routers/AppRouter';
 
 const init = () => {
-  return JSON.parse(localStorage.getItem('user') || { logged: false });
+  return localStorage.getItem('user') || { logged: false }; 
+  /* return JSON.parse(localStorage.getItem('user') || { logged: false }); */
 };
 
 export const HeroresApp = () => {
